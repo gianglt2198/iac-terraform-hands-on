8a8560b15bcfb806f0edac5e0bd26e2898436a8e
gianglt2198 <gianglt2198@gmail.com>
2025-02-17 04:33:14 +0700

    demo load config from vault

--- variables.tf
+++ variables.tf
@@ -1,11 +1,37 @@
 variable "aws_environment" {
-  type    = string
+  type = string
+
+  validation {
+    condition     = contains(["development", "staging", "production"], lower(var.aws_environment))
+    error_message = "You must use an available environment"
+  }
+
   default = "development"
 }
 
 variable "aws_region" {
   type    = string
   default = "us-east-1"
+}
+
+variable "ip_address" {
+  type = string
+
+  validation {
+    condition     = can(regex("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$", var.ip_address))
+    error_message = "Must be an IP address of the form X.X.X.X"
+  }
+
+  default = "10.0.0.0"
+}
+
+variable "phone_number" {
+  type = string
+
+  validation {
+    condition     = length(var.phone_number) == 10
+    error_message = "Phone number limit 10 characters"
+  }
 }
 
 variable "vpc" {
