6ee31058fa0bf5d3c62d6907cc807cb0744d6cf0
giang.le <giang.le@sungrove.co.jp>
2025-02-16 08:44:16 +0700

    demo module

--- main.tf
+++ main.tf
@@ -39,6 +39,27 @@ locals {
   service_name = "ec2-${var.aws_environment}-${var.aws_region}"
 }
 
+module "subnet_addrs" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+
+  base_cidr_block = "10.0.0.0/22"
+  networks = [
+    {
+      name     = "module_network_a"
+      new_bits = 2
+    },
+    {
+      name     = "module_network_b"
+      new_bits = 2
+    }
+  ]
+}
+
+output "subnet_addrs" {
+  value = module.subnet_addrs.network_cidr_blocks
+}
+
 # Define the VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc.cidr
