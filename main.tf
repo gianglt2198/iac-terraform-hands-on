e8a6fb5bf6accd5e6048a7f410d44822308e744f
giang.le <giang.le@sungrove.co.jp>
2025-02-16 10:08:53 +0700

    demo local and remote execution in ec2

--- main.tf
+++ main.tf
@@ -22,16 +22,16 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-data "aws_ami" "ubuntu_22_04" {
-  most_recent = true
+# data "aws_ami" "ubuntu_22_04" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
 
-  owners = ["099720109477"]
-}
+#   owners = ["099720109477"]
+# }
 
 locals {
   team         = "api_dev"
@@ -163,13 +163,60 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
+# Associate public subnets with public route table  
+resource "aws_route_table_association" "public_subnet_association" {
+  for_each = var.public_subnets
+
+  subnet_id      = aws_subnet.public_subnets[each.key].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Associate private subnets with private route table  
+resource "aws_route_table_association" "private_subnet_association" {
+  for_each = var.private_subnets
+
+  subnet_id      = aws_subnet.private_subnets[each.key].id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
 # Create EC2 Instance in public subnet
 resource "aws_instance" "web_server" {
-  ami                         = data.aws_ami.ubuntu_22_04.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups             = [aws_security_group.demo_security_group.id]
+  security_groups             = [aws_security_group.sg_ssh.id, aws_security_group.sg_web_ping.id, aws_security_group.sg_web_traffic.id]
   associate_public_ip_address = true
+  key_name                    = aws_key_pair.key_pair_generated.key_name
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.tls_generated.private_key_pem
+    host        = self.public_ip
+    timeout     = "4m"
+  }
+
+  lifecycle {
+    ignore_changes = [security_groups]
+  }
+
+  # Provisoner to access to instance
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+  }
+
+  # Add this to ensure instance is fully initialized  
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /tmp",
+      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+      "sudo sh /tmp/assets/setup-web.sh"
+    ]
+  }
+
   tags = {
     Name      = local.service_name
     Owner     = local.team
@@ -180,13 +227,13 @@ resource "aws_instance" "web_server" {
 
 # Create random id 
 resource "random_id" "randomness" {
-  byte_length = 16
+  byte_length = 8
 }
 
 
 # Create bucket S3 
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "demo_bucket-${random_id.randomness.hex}"
+  bucket = "demo-bucket-${lower(random_id.randomness.hex)}"
 
   tags = {
     Name      = "demo_bucket"
@@ -220,4 +267,92 @@ resource "aws_security_group" "demo_security_group" {
     Terraform = "true"
   }
 
+}
+
+# Create a self-signed certificate with TLS provider
+resource "tls_private_key" "tls_generated" {
+  algorithm = "RSA"
+}
+
+locals {
+  local_key_name = "myawskey"
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.tls_generated.private_key_pem
+  filename = "${local.local_key_name}.pem"
+}
+
+resource "aws_key_pair" "key_pair_generated" {
+  key_name   = local.local_key_name
+  public_key = tls_private_key.tls_generated.public_key_openssh
+
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
+
+# Security Group to allow SSH to instance
+resource "aws_security_group" "sg_ssh" {
+  name   = "allow-all-ssh"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    description = "ssh from anywhere"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+# Security Group for Web Traffic
+resource "aws_security_group" "sg_web_traffic" {
+  name        = "web-traffic-${terraform.workspace}"
+  vpc_id      = aws_vpc.vpc.id
+  description = "Web Traffic"
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  egress {
+    description = "allow all ip and port outbounds"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_web_ping" {
+  name        = "web-ping"
+  vpc_id      = aws_vpc.vpc.id
+  description = "ICMP for Ping Access"
+  ingress {
+    description = "allow icmp traffic"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- providers.tf
+++ providers.tf
@@ -1,4 +1,18 @@
 
 terraform {
   required_version = "> 1.10.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
 }
